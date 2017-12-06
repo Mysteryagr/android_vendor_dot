DirtyUnicorns/android_vendor_du

Common sepolicy
Change-Id: I28a4aaf6c126535f0a88001582641b234a750015

sepolicy/sepolicy.mk
@@ -1,0 +1,10 @@
+BOARD_SEPOLICY_DIRS += \
+    vendor/du/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te \
+    genfs_contexts \
+    installd.te \
+    mac_permissions.xml \
+	vold.te

Mysteryagr/android_vendor_dot

sepolicy/sepolicy.mk
@@ -1,0 +1,10 @@
+BOARD_SEPOLICY_DIRS += \
+    vendor/dot/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    file.te \
+    genfs_contexts \
+    installd.te \
+    mac_permissions.xml \
+	vold.te