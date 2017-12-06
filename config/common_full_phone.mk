602b27b1ef72a565674ac55716ea77118b87622d
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-12-06 19:48:41 +0000

    fix path

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,11 +3,11 @@ $(call inherit-product, vendor/dot/config/common.mk)
 
 # World APN list
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/dot/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # World SPN overrides list
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/dot/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 # Selective SPN list for operator number who has the problem.
 PRODUCT_COPY_FILES += \
