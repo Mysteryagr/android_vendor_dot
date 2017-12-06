AOSiP/platform_vendor_aosip

Add init and mkshrc
Change-Id: Idf0ea2b206b777a429b990bdb7bd1268cd44328d

config/common.mk
@@ -22,5 +22,13 @@ PRODUCT_COPY_FILES += \
     vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/init.local.rc:root/init.aosip.rc
+
+# Don't export PS1 in /system/etc/mkshrc.
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
 # Packages
 include vendor/aosip/config/packages.mk

Mysteryagr/android_vendor_dot

config/common.mk
@@ -22,5 +22,13 @@ PRODUCT_COPY_FILES += \
     vendor/dot/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
     vendor/dot/prebuilt/common/bin/sysinit:system/bin/sysinit
 
+# Init file
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/init.local.rc:root/init.custom.rc
+
+# Don't export PS1 in /system/etc/mkshrc.
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/mkshrc:system/etc/mkshrc
+
 # Packages
 include vendor/dot/config/packages.mk