nychitman1/android_vendor_du

Include common overlays
Change-Id: I3b7682ab9fcdab1018bf2be1c48b06e5e21c5391

config/common.mk
@@ -52,3 +52,6 @@ include vendor/du/config/branding.mk
 
 # Google sounds
 include vendor/du/google/GoogleAudio.mk
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common

Mysteryagr/android_vendor_dot

config/common.mk
@@ -37,3 +37,6 @@ include vendor/dot/config/packages.mk
 
 # Branding
 include vendor/dot/config/branding.mk
+
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/dot/overlay/common