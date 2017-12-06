2ca25f726dcb713d8dc0d5b324be3d165f4a8623
ganeshi4u <ganeshvarmai4u@gmail.com>
2017-12-06 11:35:15 +0000

    give a proper zip naming

--- config/branding.mk
+++ config/branding.mk
@@ -1,8 +1,9 @@
 # Set all versions
-DOT_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-dot
-DOT_MOD_VERSION := dot_$(TARGET_PRODUCT)-$(DOT_VERSION)
+CUSTOM_BUILD_DATE := $(shell date -u +%Y%m%d)
+DOT_VERSION := DOT_$(CUSTOM_BUILD)-$(PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.dot.version=$(DOT_MOD_VERSION)
+    ro.dot.version=$(DOT_VERSION) \
+    ro.mod.build_date=$(CUSTOM_BUILD_DATE)
 
