AOSiP/platform_vendor_aosip

Add basic vendor
Change-Id: Ibe9903c5cf1d3f235944a142db9932318185021a

config/common.mk
@@ -1,0 +1,26 @@
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1
+
+PRODUCT_COPY_FILES += \
+    vendor/aosip/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/aosip/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Packages
+include vendor/aosip/config/packages.mk

Mysteryagr/android_vendor_dot

config/common.mk
@@ -1,0 +1,26 @@
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    dalvik.vm.debug.alloc=0 \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.build.selinux=1
+
+PRODUCT_COPY_FILES += \
+    vendor/dot/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/dot/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Packages
+include vendor/dot/config/packages.mk